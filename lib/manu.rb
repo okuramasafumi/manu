ab850702fb531a8d2a07674d0071d8eecfe80504
OKURA Masafumi <masafumi.o1988@gmail.com>
2025-07-02 23:43:53 +0900

    Initial commit

---
+++ lib/manu.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "manu/version"
+
+module Manu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/manu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Manu
+  VERSION = "0.1.0"
+end
